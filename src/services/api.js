929f2bd3cd7a0980517f3fe7126bf72c92b7302f
DanielAMNfiskal <141164053+DanielAMNfiskal@users.noreply.github.com>
2023-11-27 08:49:24 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:5000/",
+    baseURL: "https://apicriadocstologado.onrender.com/",  //"http://127.0.0.1:5000/",
     timeout: 3000,
     headers: { "Content-Type": "application/json" },
   });
